de70db8d1b13ceebeb28f3a1c515e03e0fbc36ce
Victor Bethencourt <victolo500@gmail.com>
2019-09-18 11:02:37 +0100

    firts commit

---
+++ src/main/java/dad/maven/Main.java
@@ -0,0 +1,12 @@
+package dad.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+	
+	public static void name(String[] args) {
+		String invertido = StringUtils.reverse("Hola Maven!");
+		System.out.println(invertido);
+		
+	}
+}
